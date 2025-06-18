a2264520f9272f81030bb54998c851268f8ebdac
cata1112 <catalin.ciorici.2008@gmail.com>
2025-06-18 12:16:03 +0000

    Fixed Firebase authentication configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,15 @@
-// Firebase configuration for manager-venituri3
+// Updated Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCX9mMtVjoW0GeG9xFTcCideyyqQK0R6QM",
   authDomain: "manager-venituri3.firebaseapp.com",
+  databaseURL: "https://manager-venituri3-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "manager-venituri3",
-  storageBucket: "manager-venituri3.firebasestorage.app",
+  storageBucket: "manager-venituri3.appspot.com",
   messagingSenderId: "407482064898",
-  appId: "1:407482064898:web:ce18efe85f685cf03a471b",
-  measurementId: "G-JK7QT9TXRY"
+  appId: "1:407482064898:web:ce18efe85f685cf03a471b"
 };
 
-// Initialize Firebase only once
-if (typeof firebase === 'undefined') {
-  console.warn('Firebase SDK not loaded');
-} else if (!firebase.apps.length) {
+// Proper initialization
+if (typeof window !== 'undefined' && !firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
